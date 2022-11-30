aa9808c8d28ed6d85e8412424ec7a7c8514f1208
roubaozibaba <442706953@qq.com>
2022-11-30 19:21:30 +0800

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+# TinaAddons
+Add T113 Chip to Official Allwinner D1-H Tina SDK
+
+## Get Started.
+
+1. Download this repo and patches at root of Tina Linux
+```shell
+git clone https://github.com/YuzukiHD/TinaAddons.git
+```
+
+2. Place files into the root of Tina Linux
+```shell
+cp -rf TinaAddons/* .
+```
+
+3. Apply patches
+```shell
+chmod 777 apply_patch.sh
+./apply_patch.sh
+```
+
+![image](https://user-images.githubusercontent.com/12003087/187932375-d23fed44-f571-4288-8e3b-f8ece9ab4951.png)
---
+++ apply_patch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export SOURCE_PATH=$(pwd)
+
+echo -e "\n**********************************\nPrepare Environment... **!!!ROOT Require!!!**\n**********************************\n"
+sudo apt-get install build-essential subversion git libncurses5-dev zlib1g-dev gawk flex quilt libssl-dev xsltproc libxml-parser-perl mercurial bzr ecj cvs unzip lib32z1 lib32z1-dev lib32stdc++6 libstdc++6 libc6:i386 libstdc++6:i386 lib32ncurses5 lib32z1 -y
+
+echo -e "\n**********************************\nApply T113 Patch...\n***********************************\n"
+
+FILE=$SOURCE_PATH/rules.mk
+if [ -f "$FILE" ]; then
+  rm -rf .gitignore
+  git apply 0001-add-t113-platform-to-d1-h-sdk.patch
+  echo -e "\n**********************************\nFixing Patch...\n**********************************\n"
+
+  rm -rf $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
+  echo -e "// Copyright 2010 Google Inc. All Rights Reserved." > $SOURCE_PATH/lichee/brandy-2.0/spl/include/openssl/assert.h
+  
+  echo -e "\n**********************************\nFixing Patch Done.\n**********************************\n"
+
+  echo -e "\n**********************************\nAdd Toolchains...\n**********************************\n"
+  wget https://github.com/YuzukiHD/TinaAddons/releases/download/1.0/arm-toolchains.tar.gz
+  tar xf arm-toolchains.tar.gz
+
+  echo -e "\n**********************************\nDone, Start Your Tina Linux\n**********************************\n"
+  rm -rf 0001-add-t113-platform-to-d1-h-sdk.patch
+  rm -rf arm-toolchains.tar.gz
+  rm -rf apply_patch.sh
+  rm -rf TinaAddons
+  rm -rf README.md
+
+else
+  echo -e "ERROR: NOT THE ROOT OF TINA LINUX!"
+fi
